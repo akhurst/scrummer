725d8ed5c274bf126bed25846f1721d08c2325b9
Allen Hurst <allenhurst@gmail.com>
2013-03-11 10:08:45 -0500

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scrummer::Application.config.secret_token = '4b12fc352fbf1a17e035d215deb66b7411f9ae5de1fd9ea369f5fa4868393d2111d69488977e8921753a579624b31f6f397fa1ace59623d1f602160c07f3a00a'
